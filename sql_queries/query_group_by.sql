400eb0bd25b8825dd1bc7469abb2e732c9e2f079
Derek Fintel <s542635@nwmissouri.edu>
2025-02-14 20:43:46 -0600

    testing

--- sql_queries/query_group_by.sql
+++ sql_queries/query_group_by.sql
@@ -1,13 +1,13 @@
 -- query_group_by.sql
 
 -- Get the number of books per author
-SELECT authors.name, COUNT(books.book_id) AS book_count
+SELECT name, COUNT(books.book_id) AS book_count
 FROM authors
 LEFT JOIN books ON authors.author_id = books.author_id
-GROUP BY authors.name;
+GROUP BY name;
 
 -- Get the average publication year of books per author
-SELECT authors.name, AVG(books.year_published) AS avg_year
+SELECT name, AVG(books.year_published) AS avg_year
 FROM authors
 LEFT JOIN books ON authors.author_id = books.author_id
-GROUP BY authors.name;
+GROUP BY name;
--- sql_queries/query_join.sql
+++ sql_queries/query_join.sql
@@ -1,11 +1,11 @@
 -- query_join.sql
 
 -- Get books with their authors (INNER JOIN)
-SELECT books.title, authors.name
+SELECT books.title, name
 FROM books
 INNER JOIN authors ON books.author_id = authors.author_id;
 
 -- Get all authors and their books (LEFT JOIN)
-SELECT authors.name, books.title
+SELECT name, books.title
 FROM authors
 LEFT JOIN books ON authors.author_id = books.author_id;
